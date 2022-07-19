0024b3243ab45de7df1dc8713b2ade1691f75cf3
Fu Zhengyu <zhengfuaj@gmail.com>
2022-07-18 21:35:44 -0400

    add double integrator dynamics

--- include/GenerateRandomProblem.h
+++ include/GenerateRandomProblem.h
@@ -61,4 +61,29 @@ inline tpl::DynamicsLinearApproximation<Scalar, 4, 2> getFixedDynamics() {
   return dynamics;
 }
 
+template <typename Scalar, int Dim>
+inline tpl::DynamicsLinearApproximation<Scalar, 2 * Dim, Dim> getDoubleIntegratorDynamicsInDimN() {
+  static_assert(Dim >= 1);
+
+  tpl::DynamicsLinearApproximation<Scalar, 2 * Dim, Dim> dynamics;
+  constexpr scalar_t dt = 0.1;
+  dynamics.A << matrix_s_t<Scalar>::Identity(Dim, Dim), dt * matrix_s_t<Scalar>::Identity(Dim, Dim), matrix_s_t<Scalar>::Zero(Dim, Dim),
+      matrix_s_t<Scalar>::Identity(Dim, Dim);
+
+  dynamics.B << 0.5 * dt * dt * matrix_s_t<Scalar>::Identity(Dim, Dim), dt * matrix_s_t<Scalar>::Identity(Dim, Dim);
+  return dynamics;
+}
+
+template <typename Scalar, int Dim>
+inline tpl::CostApproximation<Scalar, 2 * Dim, Dim> getDoubleIntegratorCostInDimN() {
+  static_assert(Dim >= 1);
+
+  tpl::CostApproximation<Scalar, 2 * Dim, Dim> cost;
+  cost.setZero();
+
+  cost.Q.diagonal() << vector_s_t<Scalar>::Ones(Dim), 0.1 * vector_s_t<Scalar>::Ones(Dim);
+  cost.R.diagonal() << 0.3 * vector_s_t<Scalar>::Ones(Dim);
+  return cost;
+}
+
 }  // namespace pdal
